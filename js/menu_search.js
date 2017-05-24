0c5eff5af30d9964ad0bc595fe2833b5dc800a34
Zadalian <christopher.munier@gmail.com>
2017-05-24 20:06:07 +0100

    Menu

--- js/menu_search.js
+++ js/menu_search.js
@@ -1,28 +1,33 @@
-	function afficher_cross() {
+function afficher_cross() {
  
-$("#burger").css('display','none');
-$( "#cross" ).css( "display", "block" );
+	$("#burger").css('display','none');
+	$( "#cross" ).css( "display", "block" );
+	$(".menu").css("display", "block");
+	cache_search();
  
 }
 
-	function afficher_burger() {
+function afficher_burger() {
  
-$("#burger").css('display','block');
-$( "#cross" ).css( "display", "none" );
+	$("#burger").css('display','block');
+	$( "#cross" ).css( "display", "none" );
+	$(".menu").css("display", "none");
+	
  
 }
 function afficher_search() {
  
-$(".clic_search").css("display", "block" );
-$("#search2").css('display','block');
-$("#search").css('display','none');
- 
+	$(".clic_search").css("display", "block" );
+	$("#search2").css('display','block');
+	$("#search").css('display','none');
+	afficher_burger();
+	 
 }
 
 function cache_search() {
  
-$(".clic_search").css("display", "none" );
-$("#search").css('display','block');
-$("#search2").css('display','none');
+	$(".clic_search").css("display", "none" );
+	$("#search").css('display','block');
+	$("#search2").css('display','none');
  
 }
